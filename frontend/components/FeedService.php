250057de73f742e78d6cec8fdb9002b84848de6e
alexey-ringo <alexey.ringo@gmail.com>
2018-11-21 14:03:49 +0000

    create post and feed-news

--- frontend/components/FeedService.php
+++ frontend/components/FeedService.php
@@ -4,6 +4,7 @@ namespace frontend\components;
 
 use yii\base\Component;
 use yii\base\Event;
+use frontend\models\Feed;
 
 /**
  * Feed component
@@ -13,9 +14,26 @@ use yii\base\Event;
 class FeedService extends Component {
     
     public function addToFeeds(Event $event) {
-        echo '<pre>';
-        print_r($event);
-        echo '<pre>';
-        die('add post to feeds');
+        
+        $user = $event->getUser(); //frontend\models\events\PostCreateEvent
+        $post = $event->getPost();
+        
+        $followers = $user->getFollowers(); //frontend\models\User
+        
+        foreach($followers as $follower) {
+            $feedItem = new Feed();
+            
+            $feedItem->user_id = $follower['id'];
+            $feedItem->author_id = $user->id;
+            $feedItem->author_name = $user->username;
+            $feedItem->author_nickname = $user->getNickname();
+            $feedItem->author_picture = $user->getPicture();
+            $feedItem->post_id = $post->id;
+            $feedItem->post_filename = $post->filename;
+            $feedItem->post_description = $post->description;
+            $feedItem->post_created_at = $post->created_at;
+            
+            $feedItem->save();
+        }
     }
 }
