c631ee62180e7ef97c3ed30fefa5cee4e78ab8d3
alexey-ringo <alexey.ringo@gmail.com>
2018-12-06 20:20:51 +0000

    maxFileSize = 4 M

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -2,7 +2,8 @@
 return [
     'adminEmail' => 'admin@example.com',
     
-    'maxFileSize' => 1024 * 1024 * 2, //2 megabites
+    //'maxFileSize' => 1024 * 1024 * 2, //2 megabites
+    'maxFileSize' => 2048 * 2048 * 4, //2 megabites
     'storagePath' => '@frontend/web/uploads/', //Папка для хранения загруженных файлов
     'storageUri' =>  '/uploads/', //Адрес ресурса, по которому изображения будут доступны извне
     //Параметры для изменения размеров изображений - PostForm - Intervention\Image\ImageManager
